805249c6bfb8df9ec97e54343227cc1c22ba34dc
ckenlin <96581920+ckenlin@users.noreply.github.com>
2022-12-19 21:25:33 +0800

    Update music.py

--- music.py
+++ music.py
@@ -3,5 +3,5 @@ from PIL import Image
 year_list={'2022','2021','2020','2019','2018'}
 option_year=st.sidebar.selectbox("選擇年度",year_list)
 type_list={'抒情歌曲':{'bb','cc'},'流行歌曲':{'d','e'}}
-option_musiclist=st.sidebar.selectbox("選擇年度",type_list)  
-
+option_musiclist=st.sidebar.selectbox("選擇類型",type_list)  
+option_music==st.selectbox("選擇音樂",type_list[option_musiclist])  
