6d0cc32cf9e71f90c03fa45a25e8985632bb313f
ckenlin <96581920+ckenlin@users.noreply.github.com>
2022-12-19 21:22:54 +0800

    Update music.py

--- music.py
+++ music.py
@@ -1,5 +1,7 @@
 import streamlit as st
 from PIL import Image
-year_list={'2022':{'抒情歌曲','流行歌曲'},'2021':{'抒情歌曲','流行歌曲'},'2020':{'抒情歌曲','流行歌曲'},'2019':{'抒情歌曲','流行歌曲'},'2018':{'抒情歌曲','流行歌曲'}}
+year_list={'2022','2021','2020','2019','2018'}
 option_year=st.sidebar.selectbox("選擇年度",year_list)
-option_musiclist=st.sidebar.selectbox("選擇年度",year_list[option_year])  
+type_list={'抒情歌曲','流行歌曲'}
+option_musiclist=st.sidebar.selectbox("選擇年度",type_list)  
+music_list={'bb','cc'}
